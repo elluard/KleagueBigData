5f8f71e467825d6a85065b537e015780d1d3cd04
leehwangchun <elluard@me.com>
2017-05-25 18:30:59 +0900

    #승부결과(승무패) 표시되도록 추가

--- src/main/scala/TeamWinLoseAnalysis.scala
+++ src/main/scala/TeamWinLoseAnalysis.scala
@@ -8,27 +8,27 @@ import org.apache.spark.rdd.RDD
 //case class SeasonBaseData(win : Int, draw : Int, lose : Int, getScore : Int, lossScore : Int)
 //case class MatchResult(year : Int, leagueTitle : String, matchID : Int, roundID : Int, matchDate : Date, matchTeam : String,
 //homeTeam : String, awayTeam : String, homeScore : Int, awayScore : Int, resultScore : String, stadiumName : String)
-case class WinLoseData(matchDate : Date, opposingTeam : String, getScore : Int, lossScore : Int, stadium : String)
+case class WinLoseData(winOrLose : String, matchDate : Date, opposingTeam : String, getScore : Int, lossScore : Int, stadium : String)
 
 class TeamWinLoseAnalysis(teamName : String, matchResultChunk : RDD[MatchResult]) extends Serializable {
   val winResult : RDD[WinLoseData] = extractWinResult(teamName, matchResultChunk)
   val loseResult : RDD[WinLoseData] = extractLoseResult(teamName, matchResultChunk)
   val drawResult : RDD[WinLoseData] = extractDrawResult(teamName, matchResultChunk)
 
-  def makeWinLoseData(teamName: String, result : MatchResult) : WinLoseData = {
-    WinLoseData(result.matchDate, result.opposingTeam(teamName), result.selfScore(teamName), result.opposingScore(teamName), result.stadiumName)
+  def makeWinLoseData(winOrLose : String, teamName: String, result : MatchResult) : WinLoseData = {
+    WinLoseData(winOrLose, result.matchDate, result.opposingTeam(teamName), result.selfScore(teamName), result.opposingScore(teamName), result.stadiumName)
   }
 
   def extractWinResult(teamName : String, matchResult : RDD[MatchResult]) : RDD[WinLoseData] = {
-    matchResult.filter(_.checkWin(teamName)).map(makeWinLoseData(teamName, _))
+    matchResult.filter(_.checkWin(teamName)).map(makeWinLoseData("Win", teamName, _))
   }
 
   def extractLoseResult(teamName : String, matchResult : RDD[MatchResult]) : RDD[WinLoseData] = {
-    matchResult.filter(_.checkLose(teamName)).map(makeWinLoseData(teamName, _))
+    matchResult.filter(_.checkLose(teamName)).map(makeWinLoseData("Lose", teamName, _))
   }
 
   def extractDrawResult(teamName : String, matchResult: RDD[MatchResult]) : RDD[WinLoseData] = {
-    matchResult.filter(_.checkDraw).map(makeWinLoseData(teamName, _))
+    matchResult.filter(_.checkDraw).map(makeWinLoseData("Draw", teamName, _))
   }
 
   private def showResultViaTeam(result : RDD[WinLoseData])(opposingTeam : String) : RDD[WinLoseData] = {
