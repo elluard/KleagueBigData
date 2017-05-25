4fae36efd4a0da96c31fb122e285b17076609c7a
leehwangchun <elluard@me.com>
2017-05-25 18:23:16 +0900

    #함수 추가

--- src/main/scala/TeamResultLoader.scala
+++ src/main/scala/TeamResultLoader.scala
@@ -8,7 +8,36 @@ import org.apache.spark.rdd.RDD
   */
 
 case class MatchResult(year : Int, leagueTitle : String, matchID : Int, roundID : Int, matchDate : Date, matchTeam : String,
-                       homeTeam : String, awayTeam : String, homeScore : Int, awayScore : Int, resultScore : String, stadiumName : String)
+                       homeTeam : String, awayTeam : String, homeScore : Int, awayScore : Int, resultScore : String, stadiumName : String) {
+  def checkWin(teamName : String) : Boolean = {
+    if(homeTeam.contains(teamName)) homeScore > awayScore
+    else awayScore < homeScore
+  }
+
+  def checkLose(teamName : String) : Boolean = {
+    if(homeTeam.contains(teamName)) homeScore < awayScore
+    else awayScore > homeScore
+  }
+
+  def checkDraw : Boolean = {
+    homeScore == awayScore
+  }
+
+  def opposingTeam(teamName : String) : String = {
+    if(homeTeam.contains(teamName)) awayTeam
+    else homeTeam
+  }
+
+  def selfScore(teamName : String) : Int = {
+    if(homeTeam.contains(teamName)) homeScore
+    else awayScore
+  }
+
+  def opposingScore(teamName : String) : Int = {
+    if(homeTeam.contains(teamName)) awayScore
+    else homeScore
+  }
+}
 
 class TeamResultLoader(inputFile : RDD[String])  {
   val resultList = inputFile.map{ line =>
@@ -18,5 +47,5 @@ class TeamResultLoader(inputFile : RDD[String])  {
       data(6), data(7), data(8).toInt, data(9).toInt, data(10), data(11))
   }
 
-  def filter = resultList.filter(_)
+  def filter = resultList.filter _
 }
